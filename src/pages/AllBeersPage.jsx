1437358e87884695f668f8172f32ac652de3fa56
Guilherme Carvalho <guicarv99@gmail.com>
2023-12-14 17:17:36 +0000

    all beers page working

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,47 @@
-function AllBeersPage() {}
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const API_URL = 'https://ih-beers-api2.herokuapp.com/beers';
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState([]);
+
+  const getBeers = async () => {
+    try {
+      const response = await axios.get(`${API_URL}`);
+      console.log(response);
+      setBeers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBeers();
+  }, []);
+
+  return (
+    <div>
+      {beers &&
+        beers.map(beer => {
+          return (
+            <div key={beer.id} className='BeerCard'>
+              <Link to={`/beers/${beer.id}`}>
+                <img
+                  className='beerImages'
+                  src={beer.image_url}
+                  alt={beer.name}
+                />
+                <h3>{beer.name}</h3>
+                <h3>{beer.tagline}</h3>
+                <h3>{beer.contributed_by}</h3>
+              </Link>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,19 @@
-function HomePage() {}
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div className='HomePage'>
+      <button>
+        <Link to='/beers'>Beers</Link>
+      </button>
+      <button>
+        <Link to='/random-beer'>Random</Link>
+      </button>
+      <button>
+        <Link to='/new-beer'>New</Link>
+      </button>
+    </div>
+  );
+}
 
 export default HomePage;
