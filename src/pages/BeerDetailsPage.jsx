3280c51b2b8492b6019e7ac760d1ff5bbb857317
Guilherme Carvalho <guicarv99@gmail.com>
2023-12-19 17:09:14 +0000

    iteration 5 completed

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,5 +1,5 @@
-import { useParams, useEffect } from 'react-router-dom';
-import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_URL_ONE = 'https://ih-beers-api2.herokuapp.com/beers';
@@ -7,35 +7,32 @@ const API_URL_ONE = 'https://ih-beers-api2.herokuapp.com/beers';
 function BeerDetailsPage() {
   const { beerId } = useParams();
 
-  const [oneBeer, setOneBeer] = useState();
-
-  const getOneBeer = async () => {
-    try {
-      const response = await axios.get(`${API_URL_ONE}${beerId}`);
-      console.log(response);
-      setOneBeer(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [oneBeer, setOneBeer] = useState({});
 
   useEffect(() => {
+    const getOneBeer = async () => {
+      try {
+        const response = await axios.get(`${API_URL_ONE}/${beerId}`);
+        console.log(response);
+        setOneBeer(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getOneBeer();
   }, [beerId]);
 
   return (
     <div>
-      {oneBeer && (
-        <div className='OneBeerCard'>
-          <img src={oneBeer.image_url} alt={oneBeer.name} />
-          <h3>{oneBeer.name}</h3>
-          <h3>{oneBeer.tagline}</h3>
-          <h3>{oneBeer.first_brewed}</h3>
-          <h3>{oneBeer.attenuation_level}</h3>
-          <h3>{oneBeer.description}</h3>
-          <h3>{oneBeer.contributed_by}</h3>
-        </div>
-      )}
+      <div>
+        <img src={oneBeer.image_url} alt={oneBeer.name} className='oneBeer' />
+        <h3>{oneBeer.name}</h3>
+        <h3>{oneBeer.tagline}</h3>
+        <h3>{oneBeer.first_brewed}</h3>
+        <h3>{oneBeer.attenuation_level}</h3>
+        <h3>{oneBeer.description}</h3>
+        <h3>{oneBeer.contributed_by}</h3>
+      </div>
     </div>
   );
 }
